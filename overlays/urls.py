e7d7cf8eb1fe876887f1b1c99e239c7366fc5e2a
Thorsten Schmitt <mail@tschmitt.eu>
2020-07-29 21:28:10 +0200

    Added InGame Overlay

--- overlays/urls.py
+++ overlays/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('<str:user_name>/ingame', views.ingame, name='ingame'),
 ]
--- overlays/views.py
+++ overlays/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.utils.translation import gettext as _
+from django.shortcuts import render, get_object_or_404
+
+from dashboard.models import *
+from overlays.models import *
 
 
-# Create your views here.
+def ingame(request, user_name):
+    user = get_object_or_404(User, username=user_name)
+    match_overlay_data = MatchOverlayData.objects.filter(user=user).first()
+    match = match_overlay_data.current_match
+    overlay_states = OverlayState.objects.filter(user=user).first()
 
-def index(request):
-    return "Overlays"
+    current_map = match_overlay_data.current_map
+    current_map_pick_team = MapBan.objects.filter(match=match, map=current_map).first().team
+
+    template_data = {
+        'match_overlay_data': match_overlay_data,
+        'match': match,
+        'current_map_pick_team': current_map_pick_team,
+
+        'overlay_states': overlay_states,
+    }
+
+    return render(request, 'ingame.html', template_data)
