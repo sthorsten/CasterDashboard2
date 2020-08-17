b75f8e9885b4e0ffe3a66204dbd3457b5dea0286
Thorsten Schmitt <mail@tschmitt.eu>
2020-08-17 12:51:21 +0200

    Updated settings

--- caster_dashboard_2/settings.py
+++ caster_dashboard_2/settings.py
@@ -118,11 +118,4 @@ REST_FRAMEWORK = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = '/media/'
+from caster_dashboard_2.static_settings import *
