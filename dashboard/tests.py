3f50bc305cb0543e272d54297a95365a849dd8df
Thorsten Schmitt <mail@tschmitt.eu>
2020-08-18 14:28:22 +0200

    Added: Profile auto-creation test

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from dashboard.models import Profile
+
+
+class TestAutoCreatedEntries(TestCase):
+    def setUp(self):
+        User.objects.create_user("testcaseuser", "testcaseuserpass")
+
+    def testProfileEntry(self):
+        user = User.objects.get(username="testcaseuser")
+        entry = Profile.objects.get(user=user)
+        self.assertIsNotNone(entry)
+        self.assertEqual(entry.confirmed, False)
+
+
