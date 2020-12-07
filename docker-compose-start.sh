86f145abd302001f9be0ded1d83e76f78b795572
Thorsten Schmitt <mail@tschmitt.eu>
2020-12-07 10:34:24 +0100

    Added clear option to Django's collectstatic command (for Docker)

--- docker-compose-start.sh
+++ docker-compose-start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 python3 manage.py migrate --settings=dashboard.settings
-#python3 manage.py runserver 0.0.0.0:8000 --settings=dashboard.settings
-python3 manage.py collectstatic --settings=dashboard.settings --noinput
+python3 manage.py collectstatic --settings=dashboard.settings --noinput --clear
 daphne -b 0.0.0.0 -p 8000 dashboard.asgi:application
