59d5ef1849bb4edb7121e0ac2c1762f5a3c08dca
Thorsten Schmitt <mail@tschmitt.eu>
2021-02-10 10:57:13 +0100

    Minor Tweaks & Fixes
    Note: Reverts 684c314 - Redirect Bug in Nuxt Auth?

--- frontend/pages/dashboard/matches/_matchID/map/_mapID/rounds.vue
+++ frontend/pages/dashboard/matches/_matchID/map/_mapID/rounds.vue
@@ -1,11 +1,57 @@
 <template>
-    <div></div>
+    <div>
+
+        <CustomCard color="primary" outline divider title="Coming Soon">
+            <template #card-body>
+
+                <span class="font-italic">
+                    This page hasn't been implemented yet
+                </span>
+
+                <!-- Neutral Face Emoji -->
+                &#128528;<br>
+
+            </template>
+        </CustomCard>
+
+    </div>
 </template>
 
 <script>
+import CustomCard from "~/components/CustomCard";
+
 export default {
     name: "Rounds",
-    layout: "match"
+    layout: "match",
+
+    data() {
+        return {}
+    },
+
+    head() {
+        return {
+            title: this.$t("navigation.rounds") + " - Caster Dashboard"
+        }
+    },
+
+    computed: {},
+
+    mounted() {
+        this.$store.commit("setPageTitle", this.$t("navigation.rounds"))
+        this.$store.commit("setPageTitleIcon", "list-ol")
+        this.$store.commit("setBreadcrumbPath", ["Dashboard", "Matches", this.$route.params.matchID, "Map", this.$route.params.mapID, "Rounds"])
+    },
+
+    async fetch() {
+
+    },
+
+    mixins: [],
+
+    components: {
+        "CustomCard": CustomCard
+    }
+
 }
 </script>
 
