d97ea3f849745ae5322a7c0d390c1ba9020b2ac4
Thorsten Schmitt <mail@tschmitt.eu>
2021-02-17 15:05:54 +0100

    Fixed Websocket URLs

--- frontend/helpers/WebsocketURL.js
+++ frontend/helpers/WebsocketURL.js
@@ -4,12 +4,30 @@
 
 export const WebsocketURL = {
     getURL(config) {
-        let baseURL = config.baseURL.split("://")[1]
+        let websocketBaseURL = ""
+        let baseURL = ""
 
-        if (location.protocol === "http:") {
-            return "ws://" + baseURL
-        } else if (location.protocol === "https:") {
-            return "wss://" + baseURL
+        if (config.baseURL){
+            baseURL = config.baseURL.split("://")[1]
+
+            if (location.protocol === "http:") {
+                websocketBaseURL = "ws://" + baseURL
+            } else if (location.protocol === "https:") {
+                websocketBaseURL =  "wss://" + baseURL
+            }
+
+        } else {
+            if (location.protocol === "http:") {
+                websocketBaseURL = "ws://" + location.host
+            } else if (location.protocol === "https:") {
+                websocketBaseURL =  "wss://" + location.host
+            }
         }
+
+        console.log("BASE_URL => ", baseURL)
+        console.log("LOCATION_HOST => ", location.host)
+        console.log("WEBSOCKET_BASE_URL => ", websocketBaseURL)
+
+        return websocketBaseURL
     }
 }
