13edb62a29cf8bb25a27a5e197a0b0d9f8b6703d
Thorsten Schmitt <mail@tschmitt.eu>
2021-03-04 14:01:20 +0100

    Set JWT Authentication as first auth class

--- caster_dashboard_2/settings.py
+++ caster_dashboard_2/settings.py
@@ -89,9 +89,9 @@ USE_TZ = True
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.TokenAuthentication',  # deprecated
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication'
     ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
