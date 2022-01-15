b87402e126bb0fd7ffa90cd6609760da4307bdb0
Thorsten Schmitt <dev@tschmitt.eu>
2022-01-15 11:40:32 +0100

    ✨ Added redis cache to Django backend

--- backend/src/caster_dashboard_2/settings.py
+++ backend/src/caster_dashboard_2/settings.py
@@ -98,6 +98,13 @@ TEMPLATES = [
     },
 ]
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379',
+    }
+}
+
 # endregion
 
 # region REST Framwork / Authentication
